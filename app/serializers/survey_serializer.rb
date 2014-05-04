d94371447d9db60e24a746b213e75998a6e49526
Alexander Kirillov <saratovsource@gmail.com>
2014-05-04 22:13:27 +0400

    Add state colors & respondents state counts in survey list (refs #5)

--- app/serializers/survey_serializer.rb
+++ app/serializers/survey_serializer.rb
@@ -1,6 +1,7 @@
 class SurveySerializer < ApplicationSerializer
   attributes :id, :title, :state, :created_at, :updated_at, :respondents, :personal_qualities
   attributes :attraction_question, :referention_question, :personal_qualities_question
+  attributes :interviews
 
   def created_at
     I18n.l(object.created_at, format: :short)
@@ -12,5 +13,12 @@ class SurveySerializer < ApplicationSerializer
 
   def personal_qualities
     object.personal_quality_list.map{|i| {text: i}}
+  end
+
+  def interviews
+    {
+      all: object.interview_forms.count,
+      completed: object.interview_forms.finished.count
+    }
   end
 end
